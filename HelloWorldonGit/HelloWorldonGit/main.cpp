f9c1a894915d6f68958e8ccccb9a133aa8774c50
tangqinxin <48038133+tangqinxin@users.noreply.github.com>
2019-04-30 15:27:42 +0800

    codesmodify
    modified at 15:27

--- HelloWorldonGit/HelloWorldonGit/main.cpp
+++ HelloWorldonGit/HelloWorldonGit/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 int main() {
 	std::cout << "hello world\n";
-	std::cout<<"modify the codes,10:37\n";
+	std::cout << "modify the codes,10:37\n";
+	std::cout<<"modify the codes,and updata at 15:26\n";
 	system("pause");
 	return 0;
 }
